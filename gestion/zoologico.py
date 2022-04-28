8ff9f9b6aa7291c223c3b8f4ac95071a1a771dee
Andres Santamaria <gasantamariac@unal.edu.co>
2022-04-27 22:11:27 -0500

    correcion método getZona()

--- gestion/zoologico.py
+++ gestion/zoologico.py
@@ -1,31 +1,31 @@
 class Zoologico:
-
     def __init__(self, nombre, ubicacion):
         self._nombre = nombre
         self._ubicacion = ubicacion
         self._zonas = []
-    
-    #métodos
+
+    # métodos
+    def getZona(self):
+        return self._zonas
+
     def agregarZonas(self, zona):
         self._zonas.append(zona)
-    
+
     def cantidadTotalAnimales(self):
         total = 0
         for zona in self._zonas:
             total += len(zona.cantidadAnimales())
         return total
-    
-    #getters & setters
+
+    # getters & setters
     def getNombre(self):
         return self._nombre
-    
+
     def setNombre(self, nombre):
         self._nombre = nombre
-    
+
     def getUbicacion(self):
         return self._ubicacion
-    
+
     def setUbicacion(self, ubicacion):
         self._ubicacion = ubicacion
-
-
